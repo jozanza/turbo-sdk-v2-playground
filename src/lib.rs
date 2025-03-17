88defd4613ea1b7b40ffe5810ed0954a15f43779
jozanza <hello@jsavary.com>
2025-03-17 11:50:17 -0400

    custom font

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 turbo::go!({
+    // If there's a custom shader set, we think it's party time!
     let is_it_lit = !shaders::get().is_empty();
-
-    if gamepad(0).start.just_pressed() {
-        audio::stop("tota");
-        shaders::reset();
-        let anim = animation::get("tank");
-        anim.use_sprite("tank_02#walk");
-    }
 
     // Get the pointer (aka mouse left button or touch)
     let p = pointer();
@@ -15,15 +9,17 @@ turbo::go!({
     // If so, set it to the attack animation.
     if p.just_pressed() {
         if is_it_lit {
-            audio::stop("tota");
-            shaders::reset();
+            // Set the animation back to walk.
             let anim = animation::get("tank");
             anim.use_sprite("tank_02#walk");
+            // Stop the tota audio loop.
+            audio::stop("tota");
+            shaders::reset();
         } else {
-            // Set the animation to attack
+            // Set the animation to attack.
             let anim = animation::get("tank");
             anim.use_sprite("tank_02#attack");
-            // Start tota loop if not already playing.
+            // Start tota audio loop if not already playing.
             if !audio::is_playing("tota") {
                 audio::play("tota");
             }
@@ -34,18 +30,25 @@ turbo::go!({
         }
     }
 
+    // Let's make it extra jazzy by displaying text with a custom font.
+    if is_it_lit {
+        text!("IT'S LIT!!!", font = "OldWizard", x = 20, y = 40);
+    }
+
     // Draw the animated sprite using the animation_key.
     // By using the animation key, the sprite name and frame
     // are set from the animation with the given key.
     // The position is based on the pointer x and y position.
     sprite!(
         animation_key = "tank",
-        default_sprite = if !shaders::get().is_empty() {
+        // When an animation completes its loop count,
+        // The animation will use the default set here.
+        default_sprite = if is_it_lit {
             "tank_02#cheer"
         } else {
             "tank_02#walk"
         },
         x = 50,
-        y = 40
+        y = 50
     );
 });
